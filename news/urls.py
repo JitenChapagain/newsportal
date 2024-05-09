897dc7c7e2c541b5674911772167d25ff1ca572f
ar-ju-n <att560112@gmail.com>
2024-05-09 13:47:42 +0545

    addslider

--- news/urls.py
+++ news/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'news'
 
 urlpatterns = [
-    path('', views.news_list, name='news_list'),
+    path('', views.index, name='index'),
+    path('news/', views.news_list, name='news_list'),
     path('<slug:slug>/', views.news_detail, name='news_detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import News
 
+def index(request):
+    news = News.objects.filter(status=True)  # Filter only active news articles
+    news_with_index = [(index, item) for index, item in enumerate(news)]
+    context = {
+        'news_with_index': [(index + 1, item) for index, item in news_with_index]
+    }
+    return render(request, 'index.html', context)
+
 def news_list(request):
     news = News.objects.all().filter(status=True)  # Filter only active news articles
     return render(request, 'news_list.html', {'news': news})
